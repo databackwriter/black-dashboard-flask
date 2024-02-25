ca4a7d3f2f5eea2b9b6a6207fca32fddada64548
databackwriter <peter.f.moore@gmail.com>
2024-02-25 10:53:45 +0000

    clean up lights panel

--- apps/services/hue.py
+++ apps/services/hue.py
@@ -1,13 +1,18 @@
 from apps.services import hue
 import pandas as pd
 
+groups = hue.get_groups()
 
+# Print light properties
+for group in groups:
+    print(group.id_)
+    print(group.name)
 
 def light__df(light_cols = ['sdk', 'id_', 'name', 'is_on', 'bri', 'hue', 'sat']):
     return pd.DataFrame([o.__dict__ for o in hue.get_lights()])[light_cols]
 
 def light_list(light_cols = ['sdk', 'id_', 'name', 'is_on', 'bri', 'hue', 'sat']):
-    return hue.get_lights()
+    return sorted(hue.get_lights(), key=lambda x: x.name, reverse=True)
 
 def toggle_light(light_id):
     x = hue.get_light(id_=light_id)
